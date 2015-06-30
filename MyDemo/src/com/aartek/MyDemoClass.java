5291b602d4daceee6b63b53afce1b105e8be40db
rajkumarverma <dhakadraj21@gmail.com>
2015-06-30 14:02:27 +0530

    add rajkumar new

--- MyDemo/src/com/aartek/MyDemoClass.java
+++ MyDemo/src/com/aartek/MyDemoClass.java
@@ -5,9 +5,14 @@ public class MyDemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	
+		System.out.println("Hello My Demo Project..................");
 		
+		System.out.println("hi......");
+
 		System.out.println("Hello My Demo Project");
 		System.out.println("first time edit");
+		System.out.println("rajkumar");
 
 	}
 
