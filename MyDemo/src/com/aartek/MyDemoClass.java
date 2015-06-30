e1ba167479b7ac6eea72be215243cd07d28afbb6
rajkumarverma <dhakadraj21@gmail.com>
2015-06-30 11:27:55 +0530

    This is My Demo Project

---
+++ MyDemo/src/com/aartek/MyDemoClass.java
@@ -0,0 +1,13 @@
+package com.aartek;
+
+public class MyDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello My Demo Project");
+
+	}
+
+}
